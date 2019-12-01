65191e30d86145d8aa082dbf47ab2acbfef62337
AdrianPaez <adriantpaez@gmail.com>
2019-12-01 13:36:09 -0500

    Parse arguments with flag package

--- src/main.go
+++ src/main.go
@@ -5,79 +5,31 @@ import (
 	"DSpotify/src/kademlia"
 	"crypto/sha1"
 	"encoding/json"
+	"flag"
 	"fmt"
-	"log"
 	"net"
 	"os"
-	"strconv"
 )
 
 func main() {
-	var idSeed, database, ipArg, knownFile string
-	var inPort, outPort, httpPort int
-	var knownContact *kademlia.Contact
-	var err error
-	for i := 1; i < len(os.Args)-1; {
-		switch os.Args[i] {
-		case "--idSeed":
-			idSeed = os.Args[i+1]
-		case "--inPort":
-			inPort, err = strconv.Atoi(os.Args[i+1])
-			if err != nil {
-				log.Println(err.Error())
-				return
-			}
-		case "--outPort":
-			outPort, err = strconv.Atoi(os.Args[i+1])
-			if err != nil {
-				log.Println(err.Error())
-				return
-			}
-		case "--httpPort":
-			httpPort, err = strconv.Atoi(os.Args[i+1])
-			if err != nil {
-				log.Println(err.Error())
-				return
-			}
-		case "--database":
-			database = os.Args[i+1]
-		case "--ip":
-			ipArg = os.Args[i+1]
-		case "--known":
-			knownFile = os.Args[i+1]
-		default:
-			log.Printf("Unexpected param %s\n", os.Args[i])
-			return
-		}
-		i = i + 2
-	}
-	if idSeed == "" {
-		println("Param --idSeed is required")
-		return
-	}
-	if inPort == 0 {
-		inPort = 8000
-	}
-	if outPort == 0 {
-		outPort = 8001
-	}
-	if httpPort == 0 {
-		httpPort = 8080
-	}
-	if database == "" {
-		database = "database.db"
-	}
-	if ipArg == "" {
-		ipArg = "127.0.0.1"
-	}
-	ip := net.ParseIP(ipArg)
+	var idSeed = flag.String("idSeed", "", "[REQUIRED] Seed for NodeID. NodeID=sha1(idSeed).")
+	var database = flag.String("database", "database.db", "Path for database.")
+	var ipArg = flag.String("ip", "127.0.0.1", "The IP of the node.")
+	var knownFile = flag.String("known", "", "File with known contact for join to network.")
+	var inPort = flag.Int("inPort", 8000, "Input port for listen all RPC requests.")
+	var outPort = flag.Int("outPort", 8001, "Output port for send all RPC requests.")
+	var httpPort = flag.Int("httpPort", 8080, "Port for listen requests to web API.")
+	flag.Parse()
+
+	ip := net.ParseIP(*ipArg)
 	if ip == nil {
 		fmt.Printf("ERROR: Invalid IP: %s\n", ipArg)
 		return
 	}
-	//runtime.GOMAXPROCS(9)
-	if knownFile != "" {
-		file, err := os.Open(knownFile)
+
+	var knownContact *kademlia.Contact
+	if *knownFile != "" {
+		file, err := os.Open(*knownFile)
 		if err != nil {
 			fmt.Println("ERROR:", err.Error())
 			return
@@ -97,14 +49,19 @@ func main() {
 		}
 		knownContact = &c
 	}
-	var key kademlia.Key = sha1.Sum([]byte(idSeed))
-	server := kademlia.NewServer(key, ip, inPort, outPort, database)
+
+	if *idSeed == "" {
+		fmt.Println("Not idSeed set.")
+		return
+	}
+
+	var key kademlia.Key = sha1.Sum([]byte(*idSeed))
+	server := kademlia.NewServer(key, ip, *inPort, *outPort, *database)
 	httpServer := http_server.HttpServer{
 		Server: *server,
 		Host:   ip,
-		Port:   httpPort,
+		Port:   *httpPort,
 	}
 	go httpServer.Start()
 	server.Start(knownContact)
-
 }
